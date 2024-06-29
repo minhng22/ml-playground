15e2c2ae46051a571075761e1b7723cc755fa7b5
minhnguyen <comrc019@icloud.com>
2024-06-29 14:30:42 -0700

    na

---
+++ scaler.py
@@ -0,0 +1,12 @@
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
+
+a = MinMaxScaler((0, 1))
+b = a.fit_transform(np.reshape([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], (10, 1)))
+
+c = a.transform(np.reshape([2, 2, 2, 2, 2, 2, 2, 2, 2, 2], (10, 1)))
+print(c)
+
+c = a.fit_transform(np.reshape([2, 2, 2, 2, 2, 2, 2, 2, 2, 2], (10, 1)))
+print(c)
